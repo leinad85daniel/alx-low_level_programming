03b2391f162c938a5d26bc9ad351448e96add287
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-09 15:15:32 +0300

    Task-6 error fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
+/*
+ * File: 6-size.c
+ * Auth: Daniel Yohannes
+ */
+
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of an long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of an long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 	return (0);
 }
-
