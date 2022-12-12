cbcc4140e61f2c57f6c75e39ad20eb59fba618cc
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-12 17:14:35 +0300

    compiling error fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ if (n > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
-else if (n % 10 == 0)
+else if (n == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
+printf("Last digit of %d is 0 and is 0", n);
 }
 else
 {
