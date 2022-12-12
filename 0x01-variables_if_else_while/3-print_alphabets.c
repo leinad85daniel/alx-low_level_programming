ca78c4de3e5d8c77b727f009c829a16020088eb1
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-12 18:40:58 +0300

    Task-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet, ALPHABET;
+alphabet = 'a';
+ALPHABET = 'A';
+
+while (alphabet <= 'z')
+{
+		putchar (alphabet);
+		alphabet++;
+
+}
+
+while (ALPHABET <= 'Z')
+{
+putchar (ALPHABET);
+ALPHABET++;
+}
+putchar ('\n');
+return (0);
+}
