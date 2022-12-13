30ddd0f83dbcc6175e6c560bd8c8bbcb15ec12fd
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-13 19:40:03 +0300

    Task-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Description - print the alphabet
+ * Return - Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+int a;
+a = 1;
+
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+
+while (a <= 10)
+{
+a++;
+}
+alphabet++;
+
+}
+_putchar('\n');
+
+}
