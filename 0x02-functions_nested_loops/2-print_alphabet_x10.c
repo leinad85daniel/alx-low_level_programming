eb5f7d36b3f6aac5f56615acf4fe53b11d2dd2db
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-13 22:22:02 +0300

    Task-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,6 @@
  *Description: print alphabet 10 times.
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
-
-int main(void)
-
-{
-print_alphabet_x10();
-return (0);
-}
 
 void print_alphabet_x10(void)
 {
