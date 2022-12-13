6adb904fb2537e8b2081877b327035be32bfc5e1
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-13 19:23:25 +0300

    Task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description - print the alphabet
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+char alphabet;
+alphabet = 'a';
+while (alphabet <= 'z')
+
+{
+_putchar('a');
+alphabet++;
+
+}
+return (0);
+}
