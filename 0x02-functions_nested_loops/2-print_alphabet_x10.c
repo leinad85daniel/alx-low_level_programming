87a9d58c330c4f54d7c7c41f46998f226099c792
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-13 20:09:14 +0300

    Task-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,37 +1,34 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_alphabet_x10 - this function prints alphabet 10 times
+ * main - this is an entry point.
+ *Description: print alphabet 10 times.
  * Return: Always 0.
  */
+void print_alphabet_x10(void);
 
 int main(void)
 {
-	print_alphabet_x10();
-	return (0);
+print_alphabet_x10();
+return (0);
 }
-
-
 
 void print_alphabet_x10(void)
 {
-
 char alphabet;
 alphabet = 'a';
 int a;
 
-
 while (a <= 10)
-{
 a = 1;
 a++;
-
+{
 while (alphabet <= 'z')
 {
 _putchar(alphabet);
+alphabet++;
 }
 }
 _putchar('\n');
-
 }
