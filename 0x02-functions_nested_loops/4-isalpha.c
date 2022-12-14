171ebfbf0272c20efde7fdaeb522a6ad972a38fe
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-14 19:10:43 +0300

    trying

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,10 +10,10 @@ int _isalpha(int c)
 {
 if ((c >= 65) && c <= 122)
 {
-    if (c >= 91 && c <= 96)
-    {
-        return (0);
-    }
+if (c >= 91 && c <= 96)
+{
+return (0);
+}
 return (1);
 }
 
