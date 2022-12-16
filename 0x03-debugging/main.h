2128c42096e27d96e3d17cede4db06ac8b9b3cad
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-16 05:08:04 +0300

    Tak-2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include "2-largest_number.c"
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)l
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
