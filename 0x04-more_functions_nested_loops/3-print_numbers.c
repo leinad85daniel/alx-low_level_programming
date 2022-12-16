66180465687512d5e1caf56b8cbbaf1ca5c4f705
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-16 12:01:55 +0300

    Task-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9
+ */
+void print_numbers(void)
+{
+
+char nums;
+nums = 0;
+
+while (nums <= '9')
+{
+_putchar(nums);
+nums++;
+
+}
+_putchar('\n');
+}
