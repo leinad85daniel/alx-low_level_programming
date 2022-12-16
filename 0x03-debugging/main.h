c1b1d1cd425f52e6db71dfc0335aa09534ed3fc3
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-16 05:37:33 +0300

    Task-3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include "3-print_remaining_days.c"
-#include "3-convert_day.c"
+
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
