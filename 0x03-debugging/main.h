554a2382f42fee392f9cc9a3cabee7f5cc85cd18
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-16 05:37:04 +0300

    Task-3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,9 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+#include "3-print_remaining_days.c"
+#include "3-convert_day.c"
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
