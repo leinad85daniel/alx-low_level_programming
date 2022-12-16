3e66c87b2c432196816768db76d3d85243aa7c0a
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-16 14:52:01 +0300

    Task-4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 to 9, skip 2 and 4
+ */
+void print_most_numbers(void)
+{
+char n;
+n = '0';
+
+while (n <= '9')
+{
+if (n != '2' && n != '4')
+
+_putchar(n);
+n++;
+
+}
+_putchar('\n');
+
+}
