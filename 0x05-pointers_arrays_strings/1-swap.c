76eb6831757c211772a387451c3cbb8dff7c819b
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-20 00:53:46 +0300

    Task-1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - rswap two numbers
+ * @a: the first number
+ * @b: the second number
+ */
+void swap_int(int *a, int *b)
+{
+
+int tmp;
+
+tmp = *a;
+*a = *b;
+*b = tmp;
+
+}
