4e06e5bb25579c98656fc7e6451da083ecdc6277
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-20 16:22:53 +0300

    Task-3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -5,3 +7,5 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
+
+void _puts(char *str);
