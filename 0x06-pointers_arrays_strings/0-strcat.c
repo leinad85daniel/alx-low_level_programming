f731ae3f87c20f8de12589c4ae8752b8b48318b9
Daniel Yohannes <leinad.sennahoy@gmail.com>
2022-12-22 22:10:03 +0300

    Task-0 trial 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
-while (*(dest + i))
-i++;
-while (*(src + j))
-{
-*(dest+i) = *(src+j);
-i++;
-j++;
-}
-*(dest+i) = '\0';
-return (dest);
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
