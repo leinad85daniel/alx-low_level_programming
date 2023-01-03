7c5d41bbc0d8aa694ed421c01b8c19d2505dff6f
Daniel Yohannes <leinad.sennahoy@gmail.com>
2023-01-04 01:09:36 +0300

    Task 6 - 9

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,10 +10,10 @@ void print_chessboard(char (*a)[8])
 	int i, j;
 
 	i = j = 0;
-	while (i < 8)
+	while (i < 9)
 	{
 		j = 0;
-		while (j < 8)
+		while (j < 9)
 		{
 			_putchar(a[i][j]);
 			j++;
