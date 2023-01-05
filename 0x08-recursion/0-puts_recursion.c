a4411010f58981f89445118bff4ec3c6eb03c4f9
Daniel Yohannes <leinad.sennahoy@gmail.com>
2023-01-05 14:08:59 +0300

    Task-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string
+ * @s: The string to be printed
+*/
+
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+}
+
