6491935544832866bc7c3866ba108924255d675f
Daniel Yohannes <leinad.sennahoy@gmail.com>
2023-01-07 03:34:04 +0300

    Adv

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,25 @@
 /**
- * wildcmp - compare if string with wildcard mattches
- * @s1: one string
- * @s2: one string
- * Return: 1 if matched, 0 if not
- */
-
+ * wildcmp - compares two strings
+ * @s1: 1st string to be compared
+ * @s2: second string containing '*' or not
+ * Return: 1 if the strings can be considered as identical, and 0
+ **/
 int wildcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-	while (s1[i] != '\0' && s2[j] != '\0')
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '\0')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == '\0')
 	{
-		if (s1[i] == s2[j])
-			i++; j++;
-		if (s2[j] == "*")
-		{
-			j++;
-			while ((s1[i] != s2[j]) && s1[i] != '\0')
-				i++;
-		}
-		if (s1[i] != s2[j])
+		if (*s2 != '*')
 			return (0);
+		else
+			return (wildcmp(s1, s2 + 1));
 	}
-	return (1);
+	return (0);
 }
